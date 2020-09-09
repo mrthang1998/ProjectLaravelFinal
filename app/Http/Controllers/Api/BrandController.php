dd55fdafbfc3d8704c544781b42c7937f4c65d1f
Thang <mrthang1998142@gmail.com>
2020-09-09 09:48:32 +0700

    api branch controller

---
+++ app/Http/Controllers/Api/BrandController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Brand;
+
+class BrandController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return api_success(
+            array('data' => Brand::all())
+        );
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $brand = Brand::findOrFail($id);
+        return api_success(
+            array('data' => $brand)
+        );
+    }
+}
